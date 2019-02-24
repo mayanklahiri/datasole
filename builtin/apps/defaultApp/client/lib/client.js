69632ec6ebca75adae3b903efc0f30ee621b0ad6
Mayank Lahiri <mayank@iceroad.io>
2019-02-24 00:48:59 -0800

    Backend server support and abstracted operations language.

--- builtin/apps/defaultApp/client/lib/client.js
+++ builtin/apps/defaultApp/client/lib/client.js
@@ -15,6 +15,7 @@ function main() {
     // Routes
     "DebugRoute",
     "HomeRoute",
+    "StdinRoute",
     "ErrorRoute",
 
     // Components
---
+++ builtin/apps/defaultApp/server/lib/main.js
@@ -0,0 +1,72 @@
+const readline = require("readline");
+const log = console;
+
+const IMAGE_NAMES = ["farmland", "wheat", "tractor", "windmill", "field"];
+
+function choice(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function main(argv) {
+  const sendFn = process.send.bind(process) || console.log;
+
+  // Send ready command.
+  sendFn({
+    type: "ready"
+  });
+
+  // Send model cleanup.
+  sendFn({
+    type: "apply",
+    ops: [{ type: "$clearAll" }]
+  });
+
+  // Send junk periodically.
+  setInterval(() => {
+    const imageList = [1, 2, 3, 4].map(() => choice(IMAGE_NAMES));
+
+    process.send({
+      type: "apply",
+      ops: [
+        {
+          type: "$set",
+          keyPath: "junk",
+          value: {
+            now: Date.now(),
+            random: Math.random(),
+            pid: process.pid
+          }
+        },
+        {
+          type: "$set",
+          keyPath: "imageList",
+          value: imageList
+        }
+      ]
+    });
+  }, 2500);
+
+  // Read standard input lines and stream to circular buffer.
+  const rl = readline.createInterface({
+    input: process.stdin
+  });
+  rl.on("line", line => {
+    process.send({
+      type: "apply",
+      ops: [
+        {
+          type: "$circularAppend",
+          keyPath: "stdin.lines",
+          value: line,
+          maxSize: 100
+        }
+      ]
+    });
+  });
+}
+
+if (require.main === module) {
+  return main(process.argv.slice(2));
+} else {
+  module.exports = main;
+}
